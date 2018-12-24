ec4d8e8dd0c387a87b2fc726c91e16cabb6950a8
ivso0001 <ivso0001@naver.com>
2018-12-25 03:41:17 +0900

    Update index.js
    Uncaught Error: ko locale is currently unavailable. Please check the progress of converting this locale to v2.0.0 in this issue on Github
    수정

--- src/locale/ko/_lib/localize/index.js
+++ src/locale/ko/_lib/localize/index.js
@@ -1,35 +1,169 @@
-import buildLocalizeFn from '../../../_lib/buildLocalizeFn/index.js'
-import buildLocalizeArrayFn from '../../../_lib/buildLocalizeArrayFn/index.js'
+'use strict';
 
-var weekdayValues = {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _index = require('../../../_lib/buildLocalizeFn/index.js');
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var eraValues = {
+  narrow: ['B', 'A'],
+  abbreviated: ['BC', 'AD'],
+  wide: ['Before Christ', 'Anno Domini']
+};
+
+var quarterValues = {
+  narrow: ['1', '2', '3', '4'],
+  abbreviated: ['Q1', 'Q2', 'Q3', 'Q4'],
+  wide: ['1st quarter', '2nd quarter', '3rd quarter', '4th quarter']
+};
+
+// Note: in English, the names of days of the week and months are capitalized.
+// If you are making a new locale based on this one, check if the same is true for the language you're working on.
+// Generally, formatted dates should look like they are in the middle of a sentence,
+// e.g. in Spanish language the weekdays and months should be in the lowercase.
+var monthValues = {
+  narrow: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+  abbreviated: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+  wide: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+};
+
+var dayValues = {
   narrow: ['일', '월', '화', '수', '목', '금', '토'],
   short: ['일', '월', '화', '수', '목', '금', '토'],
-  long: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일']
-}
+  abbreviated: ['일', '월', '화', '수', '목', '금', '토'],
+  wide: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일']
+};
 
-var monthValues = {
-  long: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월']
-}
+var dayPeriodValues = {
+  narrow: {
+    am: '오전',
+    pm: '오후',
+    midnight: 'mi',
+    noon: 'n',
+    morning: 'morning',
+    afternoon: 'afternoon',
+    evening: 'evening',
+    night: 'night'
+  },
+  abbreviated: {
+    am: '오전',
+    pm: '오후',
+    midnight: 'midnight',
+    noon: 'noon',
+    morning: 'morning',
+    afternoon: 'afternoon',
+    evening: 'evening',
+    night: 'night'
+  },
+  wide: {
+    am: '오전',
+    pm: '오후.',
+    midnight: 'midnight',
+    noon: 'noon',
+    morning: 'morning',
+    afternoon: 'afternoon',
+    evening: 'evening',
+    night: 'night'
+  }
+};
+var formattingDayPeriodValues = {
+  narrow: {
+    am: '오전',
+    pm: '오후',
+    midnight: 'mi',
+    noon: 'n',
+    morning: 'in the morning',
+    afternoon: 'in the afternoon',
+    evening: 'in the evening',
+    night: 'at night'
+  },
+  abbreviated: {
+    am: '오전',
+    pm: '오후',
+    midnight: 'midnight',
+    noon: 'noon',
+    morning: 'in the morning',
+    afternoon: 'in the afternoon',
+    evening: 'in the evening',
+    night: 'at night'
+  },
+  wide: {
+    am: '오전',
+    pm: '오후',
+    midnight: 'midnight',
+    noon: 'noon',
+    morning: 'in the morning',
+    afternoon: 'in the afternoon',
+    evening: 'in the evening',
+    night: 'at night'
+  }
+};
 
-var timeOfDayValues = {
-  long: ['오전', '오후']
-}
+function ordinalNumber(dirtyNumber, dirtyOptions) {
+  var number = Number(dirtyNumber);
 
-function ordinalNumber (dirtyNumber) {
-  var number = Number(dirtyNumber)
-  return number + '일'
+  // If ordinal numbers depend on context, for example,
+  // if they are different for different grammatical genders,
+  // use `options.unit`:
+  //
+  //   var options = dirtyOptions || {}
+  //   var unit = String(options.unit)
+  //
+  // where `unit` can be 'year', 'quarter', 'month', 'week', 'date', 'dayOfYear',
+  // 'day', 'hour', 'minute', 'second'
+
+  var rem100 = number % 100;
+  if (rem100 > 20 || rem100 < 10) {
+    switch (rem100 % 10) {
+      case 1:
+        return number + '일';
+      case 2:
+        return number + '일';
+      case 3:
+        return number + '일';
+    }
+  }
+  return number + '일';
 }
 
 var localize = {
   ordinalNumber: ordinalNumber,
-  weekday: buildLocalizeFn(weekdayValues, 'long'),
-  weekdays: buildLocalizeArrayFn(weekdayValues, 'long'),
-  month: buildLocalizeFn(monthValues, 'long'),
-  months: buildLocalizeArrayFn(monthValues, 'long'),
-  timeOfDay: buildLocalizeFn(timeOfDayValues, 'long', function (hours) {
-    return (hours / 12) >= 1 ? 1 : 0
-  }),
-  timesOfDay: buildLocalizeArrayFn(timeOfDayValues, 'long')
-}
 
-export default localize
+  era: (0, _index2.default)({
+    values: eraValues,
+    defaultWidth: 'wide'
+  }),
+
+  quarter: (0, _index2.default)({
+    values: quarterValues,
+    defaultWidth: 'wide',
+    argumentCallback: function argumentCallback(quarter) {
+      return Number(quarter) - 1;
+    }
+  }),
+
+  month: (0, _index2.default)({
+    values: monthValues,
+    defaultWidth: 'wide'
+  }),
+
+  day: (0, _index2.default)({
+    values: dayValues,
+    defaultWidth: 'wide'
+  }),
+
+  dayPeriod: (0, _index2.default)({
+    values: dayPeriodValues,
+    defaultWidth: 'wide',
+    formattingValues: formattingDayPeriodValues,
+    defaultFormattingWidth: 'wide'
+  })
+};
+
+exports.default = localize;
+module.exports = exports['default'];
